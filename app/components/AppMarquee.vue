3b1220535de2770464c0ad0dee92bdece5a7c2b0
radanpro <radan1pro@gmail.com>
2026-03-25 01:51:23 +0300

    feat: enhance AppMarquee styling for improved readability and visual consistency

--- app/components/AppMarquee.vue
+++ app/components/AppMarquee.vue
@@ -1,10 +1,11 @@
 <template>
   <div
-    class="marquee-container overflow-hidden bg-accent py-4 -rotate-2 scale-105 shadow-[0_0_50px_rgba(255,42,109,0.3)]"
+    class="overflow-hidden bg-accent py-4 -rotate-2 scale-105 shadow-[0_0_50px_rgba(255,42,109,0.3)]"
   >
     <div class="marquee-track flex whitespace-nowrap">
-      <div class="marqyee-content flex items-contener" v-for="i in 4" :key="i">
-        <span class="text-black font-display text-4xl font-black uppercase px-8"
+      <div class="flex" v-for="i in 4" :key="i">
+        <span
+          class="text-black font-tropikal text-4xl font-black uppercase px-8"
           >Web Development • Creative Coding • UI/UX Design • 3D WebGL •
         </span>
       </div>
