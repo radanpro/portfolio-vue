da5335a354976722dc89473eec7578eaf37a25d8
radanpro <radan1pro@gmail.com>
2026-03-25 01:51:08 +0300

    feat: enhance AboutSection styling for improved readability and visual consistency

--- app/components/AboutSection.vue
+++ app/components/AboutSection.vue
@@ -11,12 +11,10 @@ const words = aboutText.split(" ")
         <div class="lg:col-span-4">
           <div class="reveal stricky top-32">
             <span
-              class="text-accent-2 font-display uppercase tracking-[0.3em] text-sm"
+              class="text-accent-2 font-display uppercase tracking-[0.3em] font-bold text-xl"
               >Philosophy</span
             >
-            <h3
-              class="text-3xl font-display font-bold uppercase mt-4 leading-tight"
-            >
+            <h3 class="text-3xl font-tropikal font-bold mt-4 leading-tight">
               Merging Aesthetics <br />
               With Performance
             </h3>
@@ -25,7 +23,7 @@ const words = aboutText.split(" ")
 
         <div class="lg:col-span-8">
           <p
-            class="font-display text-4xl md:text-6xl font-bold uppercase leading-[1.1] tracking-tighter text-white/20 flex flex-wrap"
+            class="font-drizy text-4xl md:text-6xl font-bold uppercase leading-[1.1] tracking-tighter text-white/20 flex flex-wrap"
           >
             <span
               v-for="(word, i) in words"
@@ -36,22 +34,34 @@ const words = aboutText.split(" ")
             </span>
           </p>
           <div
-            class="reveal mt-16 grid grid-cols-2 md:grid-cols-3 gap-8 border-t border-white/10 pt-12"
+            class="reveal mt-16 grid grid-cols-2 md:grid-cols-3 gap-8 border-t border-white/10 pt-12 font-drizy"
           >
             <div>
-              <h4 class="text-accent-2 text-xs uppercase mb-2">Frontend</h4>
+              <h4
+                class="text-accent-2 text-xs lg:text-xl uppercase mb-2 font-tropikal"
+              >
+                Frontend
+              </h4>
               <p class="text-gray-400 text-sm italic">
                 Vue 3, Nuxt 4, Three.js, GSAP
               </p>
             </div>
             <div>
-              <h4 class="text-accent-2 text-xs uppercase mb-2">Backend</h4>
+              <h4
+                class="text-accent-2 text-xs lg:text-xl uppercase mb-2 font-tropikal"
+              >
+                Backend
+              </h4>
               <p class="text-gray-400 text-sm italic">
                 AdonisJS, Laravel, PostgreSQL
               </p>
             </div>
             <div>
-              <h4 class="text-accent-2 text-xs uppercase mb-2">DevOps</h4>
+              <h4
+                class="text-accent-2 text-xs lg:text-xl uppercase mb-2 font-tropikal"
+              >
+                DevOps
+              </h4>
               <p class="text-gray-400 text-sm italic">Linux, Docker, CI/CD</p>
             </div>
           </div>
